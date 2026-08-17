49befae651a82c14d673ba71f16aa2cc2e55d25e
Hrishikesh Ballal <hrishikeshballal@yahoo.com>
2026-08-17 11:35:56 +0100

    Bump to 0.3.13: FESTIM solubility_law default + missing-field warnings

--- src/processforge/__init__.py
+++ src/processforge/__init__.py
@@ -29,7 +29,7 @@ from .units.flash import Flash
 from .units.heater import Heater
 from .eo import EOFlowsheet, EOSolver
 
-__version__ = "0.3.2"
+__version__ = "0.3.13"
 
 __all__ = [
     "Flowsheet",
